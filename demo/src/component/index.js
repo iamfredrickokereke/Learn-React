4dd408c9a88fa7be4bc735d3fa15d7baf15783b3
Kelly online <kellyonline2016@gmail.com>
2019-08-15 13:27:57 +0100

    Simple react snippets

---
+++ demo/src/component/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Appjs extends React.Component{
+
+    render() {
+        return (
+             <div>
+                 Hello React
+             </div>
+        );
+    }
+}
+
+
+export default Appjs;
