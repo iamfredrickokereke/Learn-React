aa8cd3f5a2b0ffd67899116d21a1ad412f568019
Kelly online <kellyonline2016@gmail.com>
2020-12-03 12:25:00 +0100

    Using Props destructuring and objects

--- Study-react/cookingapp/cookapp/src/index.js
+++ Study-react/cookingapp/cookapp/src/index.js
@@ -6,29 +6,50 @@ import './index.css'
 
 // Declaring Variables
 
-const img = 'https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.png?width=300&height=315&ext=.png'
-const title = 'Image - 1'
-const author = 'Written by Fredrick Achiever'
+const firstBook = {
+  img: 'https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.png?width=300&height=315&ext=.png',
+  title: 'Image - 1',
+  author: 'Written by Fredrick Achiever'
+}
+const secondBook = {
+  img: 'https://cdn.shopify.com/s/files/1/0064/5342/8271/collections/Taschen-Atm-Navy_540x.jpg?v=1556744191',
+  title: 'Image - 2',
+  author: 'Written by Fredrick Achiever'
+}
+const thirdBook = {
+  img: 'https://cdn.shopify.com/s/files/1/0064/5342/8271/collections/Classics-of-History_540x.jpg?v=1571866175',
+  title: 'Image - 3',
+  author: 'Written by Fredrick Achiever'
+}
+// const img = 'https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.png?width=300&height=315&ext=.png'
+// const title = 'Image - 1'
+// const author = 'Written by Fredrick Achiever'
 
 function BookList() {
     return (
         <section className="booklist">
-            <Book name='Fred'/>
-            <Book />
-            <Book />
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+            <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+            <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author}/>
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+            <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+            <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author}/>
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+            <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+            <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author}/>
         </section>
     )
 }
 
-const Book = (props) => {
+const Book = ({img, title, author}) => {
 
-  console.log(props);
+  
   
     return (
         <article className="card">
             <img src={img} alt=""/>
-            <h2 style={{color:'red'}}>{title} {props.name}</h2>
-            <h1 style={{letterSpacing:'5px'}}>{author.toLocaleUpperCase()}</h1>
+            <h2 style={{color:'red'}}>{title}</h2>
+            <h1 style={{letterSpacing:'5px'}}>{author}</h1>
 
         </article>
     )
