12786ed1fa2efbbf4ed8154a747ac74db0aad1eb
Kelly online <kellyonline2016@gmail.com>
2020-12-03 11:29:39 +0100

    Remove component files

--- Study-react/cookingapp/cookapp/src/index.js
+++ Study-react/cookingapp/cookapp/src/index.js
@@ -4,43 +4,37 @@ import ReactDOM from 'react-dom';
 
 import './index.css'
 
+// Declaring Variables
+
+const img = 'https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.png?width=300&height=315&ext=.png'
+const title = 'Image - 1'
+const author = 'Written by Fredrick Achiever'
+
 function BookList() {
     return (
         <section className="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book name='Fred'/>
             <Book />
             <Book />
         </section>
     )
 }
 
-const Book = () => {
+const Book = (props) => {
+
+  console.log(props);
+  
     return (
         <article className="card">
-            <Image />
-            <Title />
-            <Author />
+            <img src={img} alt=""/>
+    <h2 style={{color:'red'}}>{title} {props.name}</h2>
+            <h1 style={{letterSpacing:'5px'}}>{author.toLocaleUpperCase()}</h1>
+
         </article>
     )
 }
 
-const Image= () => {
-    return <img src="https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.png?width=300&height=315&ext=.png" alt=""/>
-}
-const Title= () => {
-    return <h2 style={{color:'red'}}>Image - 1</h2>
-}
-const Author= () => {
-    return <h1 style={{letterSpacing:'5px'}}>Written by Fredrick Achiever</h1>
-}
+
 
 
 ReactDOM.render(<BookList />, document.getElementById('root'))
